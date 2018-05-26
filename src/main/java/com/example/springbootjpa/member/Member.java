5ffb31a39f4ad2c51a459ab945c2f2e87685856f
jkkim <feelingk_jgkim@woowahan.com>
2018-05-26 11:46:03 +0900

    #1 getBirthYear() 추가 등.

--- src/main/java/com/example/springbootjpa/member/Member.java
+++ src/main/java/com/example/springbootjpa/member/Member.java
@@ -1,11 +1,15 @@
 package com.example.springbootjpa.member;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import lombok.Data;
 
@@ -18,21 +22,41 @@ public class Member {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
-	@Column
+	@Column(columnDefinition="varchar(20)", nullable=false)
 	private String name;
+	
+	@Column(length=14, nullable=false, updatable=false)
+	private String telephone;
 	
 	@Column
 	private int age;
 	
+	private Date regDate;
+	
+	@Transient	// DB에 저장되지 않음.
+	private int birthYear;
+	
 	public Member() {}
 	
 	public Member(String name, int age) {
+		this(name, "010-1234-1234", age);
+	}
+	
+	public Member(String name, String telephone, int age) {
 		this.name = name;
+		this.telephone = telephone;
 		this.age = age;
+		this.birthYear = 2018 - age;
+		this.regDate = new Date();
+	}
+	
+	public int getBirthYear() {
+		
+		return Calendar.getInstance().get(Calendar.YEAR) - this.age;
 	}
 	
 	@Override
 	public String toString() {
-		return String.format("[%d] name: %s, age: %d", id, name, age);
+		return String.format("[%d] name: %s, telephone: %s, age: %d(%d), regDate: %s", id, name, telephone, age, getBirthYear(), regDate);
 	}
 }
